87e64a8628b9eb64e6b8d657cd25ac80f57a3396
longnh1994@gmail.com <longnh1994@gmail.com@963a46f9-1d8a-dc24-590f-08096900df4a>
2014-04-17 10:12:06 +0000

    Custom font: comic san ms

--- WhereIsMyNumber/src/fu/agile/whereismynumber/Adapter/ViewHolderItem.java
+++ WhereIsMyNumber/src/fu/agile/whereismynumber/Adapter/ViewHolderItem.java
@@ -1,7 +1,9 @@
 package fu.agile.whereismynumber.Adapter;
 
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ViewHolderItem {
 	public TextView textViewItem;
+	public ImageView imageViewItem;
 }
