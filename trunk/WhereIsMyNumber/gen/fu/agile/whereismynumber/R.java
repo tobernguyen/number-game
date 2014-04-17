cf02c682c8e6251ca83e6aaa37dff6bc09aaba15
longnh1994@gmail.com <longnh1994@gmail.com@963a46f9-1d8a-dc24-590f-08096900df4a>
2014-04-17 02:01:05 +0000

    Thêm menu vào MainScreen.java

--- trunk/WhereIsMyNumber/gen/fu/agile/whereismynumber/R.java
+++ trunk/WhereIsMyNumber/gen/fu/agile/whereismynumber/R.java
@@ -1091,6 +1091,7 @@ containing a value of this type.
         public static final int shape_selected=0x7f020060;
         public static final int shape_unselected=0x7f020061;
         public static final int stroke=0x7f020062;
+        public static final int x_symbol=0x7f020063;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -1108,44 +1109,49 @@ containing a value of this type.
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
-        public static final int chronometer=0x7f050042;
+        public static final int chronometer=0x7f050046;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003c;
-        public static final int decrementTypeButton=0x7f050049;
+        public static final int decrementTypeButton=0x7f05004d;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
-        public static final int easyLevelButton=0x7f050045;
+        public static final int easyLevelButton=0x7f050049;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
-        public static final int gridValue=0x7f05004b;
-        public static final int gridView=0x7f050044;
-        public static final int hardLevelButton=0x7f050047;
+        public static final int gridValue=0x7f05004f;
+        public static final int gridView=0x7f050048;
+        public static final int hardLevelButton=0x7f05004b;
         public static final int highScoreButton=0x7f05003d;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
-        public static final int incrementTypeButton=0x7f050048;
+        public static final int imageView1=0x7f050050;
+        public static final int incrementTypeButton=0x7f05004c;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
-        public static final int mediumLevelButton=0x7f050046;
+        public static final int mediumLevelButton=0x7f05004a;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
         public static final int playButton=0x7f05003e;
         public static final int playSize_radioGroup=0x7f05003f;
+        public static final int playType_decrease=0x7f050044;
+        public static final int playType_increase=0x7f050043;
+        public static final int playType_radioGroup=0x7f050042;
+        public static final int playType_random=0x7f050045;
         public static final int play_6x6_rbtn=0x7f050040;
         public static final int play_6x8_rbtn=0x7f050041;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
-        public static final int randomTypeButton=0x7f05004a;
+        public static final int randomTypeButton=0x7f05004e;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1163,7 +1169,7 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int targetNumberTextView=0x7f050043;
+        public static final int targetNumberTextView=0x7f050047;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1229,7 +1235,8 @@ containing a value of this type.
         public static final int fragment_select_difficult_level=0x7f03001e;
         public static final int fragment_select_play_type=0x7f03001f;
         public static final int grid_number=0x7f030020;
-        public static final int support_simple_spinner_dropdown_item=0x7f030021;
+        public static final int grid_number_ticked=0x7f030021;
+        public static final int support_simple_spinner_dropdown_item=0x7f030022;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
